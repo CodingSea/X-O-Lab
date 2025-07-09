ac9fddb563388baf782b9ab41404001296a5bde5
CodingSea <Fhdq2003@email.com>
2025-07-09 16:05:52 +0300

    finished Lab Exercise Part 1-3

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,37 @@
-function init()
-{
     /*-------------------------------- Constants --------------------------------*/
 
 
 
     /*---------------------------- Variables (state) ----------------------------*/
-
+    let board;
+    let turn;
+    let winner;
+    let tie;
 
 
     /*------------------------ Cached Element References ------------------------*/
-
-
+    const squareEls = document.querySelectorAll(".sqr");
+    const messageEl = document.querySelector("#message");
 
     /*-------------------------------- Functions --------------------------------*/
+    function init()
+    {
+        board = ["", "", "", "", "", "", "", "", ""];
+        turn = "X";
+        winner = false;
+        tie = false;
 
+        render();
+    }
 
+    function render()
+    {
+        
+    }
 
     /*----------------------------- Event Listeners -----------------------------*/
 
 
-
-}
 
 
 document.addEventListener("DOMContentLoaded", init);
