e1c6e497c8f21f217a1c6a8e0f84bc8d8f6959a9
CodingSea <Fhdq2003@email.com>
2025-07-09 21:36:56 +0300

    added last changes

--- scripts/app.js
+++ scripts/app.js
@@ -1,77 +1,85 @@
-    /*-------------------------------- Constants --------------------------------*/
+/*-------------------------------- Constants --------------------------------*/
 
 
 
-    /*---------------------------- Variables (state) ----------------------------*/
-    let board;
-    let turn;
-    let winner;
-    let tie;
-    const winningCombos = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-    ]
+/*---------------------------- Variables (state) ----------------------------*/
+let board;
+let turn;
+let winner;
+let tie;
+const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+];
 
 
-    /*------------------------ Cached Element References ------------------------*/
-    const squareEls = document.querySelectorAll(".sqr");
-    const messageEl = document.querySelector("#message");
+/*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll(".sqr");
+const messageEl = document.querySelector("#message");
 
-    /*-------------------------------- Functions --------------------------------*/
-    function init()
+/*-------------------------------- Functions --------------------------------*/
+function init()
+{
+    board = ["X", "O", "",
+                "", "X", "", 
+                "", "O", "X"];
+    turn = "X";
+    winner = false;
+    tie = false;
+
+    render();
+}
+
+function updateBoard()
+{
+    for(let i = 0; i < board.length; i++)
     {
-        board = ["", "", "",
-                 "", "", "", 
-                 "", "", ""];
-        turn = "X";
-        winner = false;
-        tie = false;
-
-        render();
+        squareEls.forEach(() => 
+        {
+            squareEls[i].textContent = board[i];
+        });
     }
+}
 
-    function updateBoard()
+function updateMessage()
+{
+    if(tie && winner == false)
     {
-        for(let i = 0; i < board.length; i++)
-        {
-            squareEls.forEach(() => 
-            {
-                squareEls[i].textContent = board[i];
-            });
-        }
+        messageEl.textContent = `Tie game!`;
     }
-
-    function updateMessage()
+    else if(winner)
     {
-        if(tie && winner == false)
-        {
-            messageEl.textContent = `Tie game!`;
-        }
-        else if(winner)
-        {
-            messageEl.textContent = `${turn} wins!`;
-        }
-        else
-        {
-            messageEl.textContent = `it's ${turn}'s turn`;
-        }
+        messageEl.textContent = `${turn} wins!`;
     }
-
-    function render()
+    else
     {
-        updateBoard();
-        updateMessage();
+        messageEl.textContent = `it's ${turn}'s turn`;
     }
+}
 
-    /*----------------------------- Event Listeners -----------------------------*/
+function render()
+{
+    updateBoard();
+    updateMessage();
+}
 
+function handleClick(event)
+{
+    const squareIndex = event.id;
+    console.log(squareIndex);
+}
 
-
-
+/*----------------------------- Event Listeners -----------------------------*/
 document.addEventListener("DOMContentLoaded", init);
+squareEls.forEach((event) => 
+{
+    event.addEventListener('click', () => {
+        handleClick(event.target);
+    })
+});
